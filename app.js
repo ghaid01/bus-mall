5f286718e224fa17dc779ea900c6c264ba638d84
ghaid01 <ghaid.zahran2001@gmail.com>
2019-10-21 14:00:06 +0300

    complete

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
-
+var leftImgElem=null;
+var centerImgElem=null;
+var rightImgElem=null;
 
 function Product(name, src) {
     this.name = name;
@@ -8,9 +10,25 @@ function Product(name, src) {
     this.shownCount = 0;
     Product.all.push(this);
 }
+Product.roundCount = 0;
+Product.roundLimit = 10;
+
 
 Product.all = [];
 
+Product.container = document.getElementById('all-products');
+Product.leftImgElem = document.getElementById('left-img');
+Product.leftTextElem = document.getElementById('left-title');
+
+Product.centerImgElem = document.getElementById('center-img');
+Product.centerTextElem = document.getElementById('center-title');
+
+Product.rightImgElem = document.getElementById('right-img');
+Product.rightTextElem = document.getElementById('right-title');
+
+Product.leftProduct = null;
+Product.centerProduct = null;
+Product.rightProduct = null;
 
 // var leftimg = document.getElementById('all-products');
 // document
@@ -21,11 +39,11 @@ new Product('BOOTS', 'img/boots.jpg');
 new Product('BREAKFAST', 'img/breakfast.jpg');
 new Product('BUBBLEGUM', 'img/bubblegum.jpg');
 new Product('CHAIR', 'img/chair.jpg');
-new Product('CTHULHA', 'img/cthulha.jpg');
+new Product('CTHULHU', 'img/cthulhu.jpg');
 new Product('PET-SWEEP', 'img/pet-sweep.jpg');
 new Product('SCISSORS', 'img/scissors.jpg');
 new Product('SHARK', 'img/shark.jpg');
-new Product('SWEEP', 'img/sweep.jpg');
+new Product('SWEEP', 'img/sweep.png');
 new Product('TAUNTAUN', 'img/tauntaun.jpg');
 new Product('UNICORN', 'img/unicorn.jpg');
 new Product('USB', 'img/usb.gif');
@@ -37,69 +55,114 @@ new Product('DOG-DUCK', 'img/dog-duck.jpg');
 
 console.log(Product.all);
 
-function render() {
-    // var cotainer = document.getElementById('all-products');
-    var leftImage = document.getElementById('left-img');
-    var leftText = document.getElementById('left-img');
+// Set left, center and right global variables
+function selectRandomProducts() {
 
-    var centerImage = document.getElementById('center-img');
-    var centerText = document.getElementById('center-img');
+    var leftIndex = Math.floor(Math.random() * Product.all.length);
+    var centerIndex = Math.floor(Math.random() * Product.all.length);
+    var rightIndex = Math.floor(Math.random() * Product.all.length);
+    
+    while (leftIndex === rightIndex) {
+        rightIndex = Math.floor(Math.random() * Product.all.length);
+    }    
+    while (leftIndex === centerIndex) {
+        centerIndex = Math.floor(Math.random() * Product.all.length);
+    }    
+    while (rightIndex === centerIndex) {
+        centerIndex = Math.floor(Math.random() * Product.all.length);
+    }    
 
-    var rightImage = document.getElementById('right-img');
-    var rightText = document.getElementById('right-img');
+    Product.leftProduct = Product.all[leftIndex];
 
-    leftImage.shownCount++;
-    leftText.shownCount++;
-    centerImage.shownCount++;
-    centerText.shownCount++;
-    rightImage.shownCount++;
-    rightText.shownCount++;
+    Product.centerProduct = Product.all[centerIndex];
 
-
-
-
-
-}
-
-var leftIndex = Math.floor(Math.random() * Product.all.length);
-var leftImg = Product.all[leftIndex];
-var centerIndex = Math.floor(Math.random() * Product.all.length);
-var centerImg = Product.all[centerIndex];
-var rightIndex = Math.floor(Math.random() * Product.all.length);
-var rightImg = Product.all[rightIndex];
-
-while (leftIndex === rightIndex) {
-    rightIndex = Math.floor(Math.random() * Product.all.length);
-}
-while (leftIndex === centerIndex) {
-    centerIndex = Math.floor(Math.random() * Product.all.length);
-}
-while (rightIndex === centerIndex) {
-    centerIndex = Math.floor(Math.random() * Product.all.length);
-}
-function clickHandler(event) {
-    var clicked = event.target.id;
-    var clickedImg;
-    if (clicked === 'left-img') {
-        clickedImg = leftImg;
-    } else if (clicked === 'right-img') {
-        clickedImg = rightImg;
-    } else if (clicked === 'center-img') {
-        clickedImg = centerImg;
+    Product.rightProduct = Product.all[rightIndex];
 
     
 
-    }
-    if(clickedImg) {
+}    
+function updateTotal() {
+    var tableBody = document.getElementById('report');
+    tableBody.innerHTML = '';
+    for (var i = 0; i < Product.all.length; i++) {
+        var currentProduct = Product.all[i];
+        var row = addElement('tr', tableBody);
+        addElement('td', row, currentProduct.name);
+        addElement('td', row, '' + currentProduct.clickCount);
+        addElement('td', row, '' + currentProduct.shownCount);
+      }  
+    }  
+    function addElement(tag, container, text) {
+        var element = document.createElement(tag);
+        container.appendChild(element);
+        if(text) {
+          element.textContent = text;  
+        }  
+        return element;
+      }  
 
-        clickedImg.clickCount++;
-    }
+
+
+function render() {  
+
+    selectRandomProducts();
+
+    Product.leftImgElem.setAttribute('src', Product.leftProduct.src);
+    Product.centerImgElem.setAttribute('src', Product.centerProduct.src);
+    Product.rightImgElem.setAttribute('src', Product.rightProduct.src);
+
+    Product.leftTextElem.textContent = Product.leftProduct.name;
+    Product.centerTextElem.textContent = Product.centerProduct.name;
+    Product.rightTextElem.textContent = Product.rightProduct.name;
+
+
+    Product.leftProduct.shownCount++;
+    Product.centerProduct.shownCount++;
+    Product.rightProduct.shownCount++;
 }
+
+
+// Cannot read property 'target' of undefined
+function clickHandler(event) {
+
+    var clicked = event.target.id;
+    var clickedProduct;
+    if (clicked === 'left-img') {
+        clickedProduct = Product.leftProduct;
+    } else if (clicked === 'right-img') {
+        clickedProduct = Product.rightProduct;
+    } else if (clicked === 'center-img') {
+        clickedProduct= Product.centerProduct;
+
+
+
+    }
+    if(clickedProduct) {
+
+        clickedProduct.clickCount++;
+        Product.roundCount++;
+
+        updateTotal();
+        if(Product.roundCount == Product.roundLimit){
+            alert('You can not click anymore');
+            Product.container.removeEventlistener('click' , clickHandler);
+        } else {
+            render();
+        }
+    }
+    
+    
+    // update the shown counter
+    // check the round counter against limit
+    //          stop user clicks if over limit
+    // select new products
+    // render the new products
+}
+
+    
 
 document.getElementById('all-products').addEventListener('click', clickHandler);
 
-clickHandler();
+updateTotal();
 render();
-
-
 
