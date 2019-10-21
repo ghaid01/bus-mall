196a4007ddef5cb5cf7c7af0e3f8df5b571c68fe
ghaid01 <ghaid.zahran2001@gmail.com>
2019-10-21 08:56:03 +0300

    js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict'
 
 
-function Product(name,src){
+function Product(name, src) {
     this.name = name;
-    this.src =src;
+    this.src = src;
     this.clickCount = 0;
-    this.shownCount =0;
+    this.shownCount = 0;
     Product.all.push(this);
 }
 
@@ -14,46 +14,46 @@ Product.all = [];
 
 // var leftimg = document.getElementById('all-products');
 // document
-new Product('PEN','./img/pen.jpg');
-new Product('BANANA','img/banana.jpg');
-new Product('BATHROOM','img/bathroom.jpg');
-new Product('BOOTS','img/boots.jpg');
-new Product('BREAKFAST','img/breakfast.jpg');
-new Product('BUBBLEGUM','img/bubblegum.jpg');
-new Product('CHAIR','img/chair.jpg');
-new Product('CTHULHA','img/cthulha.jpg');
-new Product('PET-SWEEP','img/pet-sweep.jpg');
-new Product('SCISSORS','img/scissors.jpg');
-new Product('SHARK','img/shark.jpg');
-new Product('SWEEP','img/sweep.jpg');
-new Product('TAUNTAUN','img/tauntaun.jpg');
+new Product('PEN', 'img/pen.jpg');
+new Product('BANANA', 'img/banana.jpg');
+new Product('BATHROOM', 'img/bathroom.jpg');
+new Product('BOOTS', 'img/boots.jpg');
+new Product('BREAKFAST', 'img/breakfast.jpg');
+new Product('BUBBLEGUM', 'img/bubblegum.jpg');
+new Product('CHAIR', 'img/chair.jpg');
+new Product('CTHULHA', 'img/cthulha.jpg');
+new Product('PET-SWEEP', 'img/pet-sweep.jpg');
+new Product('SCISSORS', 'img/scissors.jpg');
+new Product('SHARK', 'img/shark.jpg');
+new Product('SWEEP', 'img/sweep.jpg');
+new Product('TAUNTAUN', 'img/tauntaun.jpg');
 new Product('UNICORN', 'img/unicorn.jpg');
-new Product('USB','img/usb.gif');
-new Product('WATER-CAN','img/water-can.jpg');
-new Product('WINE-GLASS','img/wine-glass.jpg');
-new Product('BAG','img/bag.jpg');
-new Product('DRAGON','img/dragon.jpg');
-new Product('DOG-DUCK','img/dog-duck.jpg');
+new Product('USB', 'img/usb.gif');
+new Product('WATER-CAN', 'img/water-can.jpg');
+new Product('WINE-GLASS', 'img/wine-glass.jpg');
+new Product('BAG', 'img/bag.jpg');
+new Product('DRAGON', 'img/dragon.jpg');
+new Product('DOG-DUCK', 'img/dog-duck.jpg');
 
 console.log(Product.all);
 
-function rendered(){
-// var cotainer = document.getElementById('all-products');
-var leftImage = document.getElementById('left-img');
-var leftText = document.getElementById('left-img');
+function render() {
+    // var cotainer = document.getElementById('all-products');
+    var leftImage = document.getElementById('left-img');
+    var leftText = document.getElementById('left-img');
 
-var centerImage = document.getElementById('center-img');
-var centerText = document.getElementById('center-img');
+    var centerImage = document.getElementById('center-img');
+    var centerText = document.getElementById('center-img');
 
-var rightImage = document.getElementById('right-img');
-var rightText = document.getElementById('right-img');
+    var rightImage = document.getElementById('right-img');
+    var rightText = document.getElementById('right-img');
 
-leftImage.shownCount++;
-leftText.shownCount++;
-centerImage.shownCount++;
-centerText.shownCount++;
-rightImage.shownCount++;
-rightText.shownCount++;
+    leftImage.shownCount++;
+    leftText.shownCount++;
+    centerImage.shownCount++;
+    centerText.shownCount++;
+    rightImage.shownCount++;
+    rightText.shownCount++;
 
 
 
@@ -61,39 +61,45 @@ rightText.shownCount++;
 
 }
 
-var leftIndex = Math.floor(Math.random()* Product.all.length);
+var leftIndex = Math.floor(Math.random() * Product.all.length);
 var leftImg = Product.all[leftIndex];
-var centerIndex = Math.floor(Math.random()* Product.all.length);
+var centerIndex = Math.floor(Math.random() * Product.all.length);
 var centerImg = Product.all[centerIndex];
-var rightIndex = Math.floor(Math.random()* Product.all.length);
+var rightIndex = Math.floor(Math.random() * Product.all.length);
 var rightImg = Product.all[rightIndex];
 
-while(leftIndex === rightIndex){
-    rightIndex = Math.floor(Math.random()* Product.all.length);
+while (leftIndex === rightIndex) {
+    rightIndex = Math.floor(Math.random() * Product.all.length);
 }
-while(leftIndex === centerIndex){
-    centerIndex = Math.floor(Math.random()* Product.all.length);
+while (leftIndex === centerIndex) {
+    centerIndex = Math.floor(Math.random() * Product.all.length);
 }
-while(rightIndex === centerIndex){
-    centerIndex = Math.floor(Math.random()* Product.all.length);
+while (rightIndex === centerIndex) {
+    centerIndex = Math.floor(Math.random() * Product.all.length);
 }
-function clickHandler(event){
+function clickHandler(event) {
     var clicked = event.target.id;
-var clickedImg;
-if(clicked === 'left-img'){
-    clickedImg = leftImg;
-}else if(clicked === 'right-img'){
-    clickedImg = rightImg;
-}else if(clicked === 'center-img'){
-clickedImg = centerImg;
+    var clickedImg;
+    if (clicked === 'left-img') {
+        clickedImg = leftImg;
+    } else if (clicked === 'right-img') {
+        clickedImg = rightImg;
+    } else if (clicked === 'center-img') {
+        clickedImg = centerImg;
 
+    
+
+    }
+    if(clickedImg) {
+
+        clickedImg.clickCount++;
+    }
 }
-clickedImg.clickCount++;
-}
 
-document.getElementById('all-products').addEventListener('click',clickHandler);
+document.getElementById('all-products').addEventListener('click', clickHandler);
 
+clickHandler();
+render();
 
-rendered();
 
 
